c396002cf701aa5a956716d4d442e147139e2eb7
rthai <ahnamiko@gmail.com>
2018-10-14 22:10:15 -0700

    implement axios get trending

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './styles/App.css';
+import Nav from './Nav';
+
+// TODO: make an error page
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      gifs: [],
+    }
+  }
+
+  componentDidMount() {
+    const endpoint = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&limit=25&rating=G`;
+
+    axios.get(endpoint)
+      .then(response => this.setState({
+        gifs: response.data.data
+      }))
+      .catch(err => console.error(err));
+  }
+
   render() {
     return (
       <div className="App">
+        <Nav />
       </div>
     );
   }
