c63c7a3c4f6d2686bbaee98152794ce59fb96e4a
rthai <ahnamiko@gmail.com>
2018-10-16 00:53:45 -0700

    define mobile drop menu nav

---
+++ src/components/DropNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/DropNav.css';
+
+const DropNav = () => (
+  <ul className="drop-menu">
+    <li className="link"><a>Home</a></li>
+    <li className="link"><a>Favorites</a></li>
+    <li className="link"><a>Upload</a></li>
+  </ul>
+);
+
+export default DropNav;
