66a137ca88704a5dc4c5d20514dd26fb14557173
rthai <ahnamiko@gmail.com>
2018-10-16 00:54:36 -0700

    add mobile menu hamburger

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,12 +5,17 @@ import Search from './Search';
 const Nav = () => (
   <nav>
     <ul className="nav-links">
-      <li>
-        <h1 className="logo"><a>GIF Search</a></h1>
+      <li className="hamburger">
+        <button className="menu">
+          <i className="material-icons icn-menu">menu</i>
+        </button>
       </li>
-      <li><a>Home</a></li>
-      <li><a>Favorites</a></li>
-      <li><a>Upload</a></li>
+      <li className="logo">
+        <h1><a>GIF Search</a></h1>
+      </li>
+      <li className="link"><a>Home</a></li>
+      <li className="link"><a>Favorites</a></li>
+      <li className="link"><a>Upload</a></li>
       <li>
         <div className="search-container">
           <Search/>
