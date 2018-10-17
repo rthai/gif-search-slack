4f4f0d0176e5de0f6c9c5cf29da059d01fad6d6e
rthai <ahnamiko@gmail.com>
2018-10-16 20:57:36 -0700

    restructure giphy data, implement sorting

--- src/components/App.js
+++ src/components/App.js
@@ -14,17 +14,102 @@ class App extends Component {
     super()
     this.state = {
       gifs: [],
+      favorites: {},
     }
+
+    this.onSort = this.onSort.bind(this);
+    // this.onSelectImage = this.onSelectImage.bind(this);
   }
 
   componentDidMount() {
     const endpoint = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&limit=25&rating=G`;
 
     axios.get(endpoint)
-      .then(response => this.setState({
-        gifs: response.data.data
-      }))
+      .then(response => {
+        let data = response.data.data;
+
+        let gifs = data.map(gif => {
+          let copy = {};
+          copy.src = gif.images.original.url;
+          copy.thumbnail = gif.images.fixed_width.url;
+          // copy.data_src = gif.images.fixed_width.url;
+          // copy.thumbnail = '';
+          copy.thumbnailWidth = parseInt(gif.images.fixed_width.width);
+          copy.thumbnailHeight = parseInt(gif.images.fixed_width.height);
+          copy.caption = gif.title;
+          copy.id = gif.id;
+          copy.user = gif.user;
+          copy.upload = gif.import_datetime;
+          return copy;
+        });
+        this.setState({ gifs });
+      })
       .catch(err => console.error(err));
+  }
+
+  // onSelectImage(index) {
+  //   var copyFavs = {...this.state.favorites};
+  //   var images = this.state.gifs.slice();
+  //   var img = images[index];
+    
+  //   if (img.hasOwnProperty("isSelected")) {
+  //     img.isSelected = !img.isSelected;
+  //   } else {
+  //     img.isSelected = true;
+  //   }
+
+  //   if (img.isSelected) {
+  //     copyFavs[index] = img
+  //   } else {
+  //     delete copyFavs[index]
+  //   }
+  //   this.setState(
+  //     {favorites: copyFavs}, 
+  //     () => localStorage.setItem('favorites', JSON.stringify(this.state.favorites))
+  //   );
+  // }
+
+  onSort(e) {
+    const data = this.state.gifs.slice();
+    let input = e.target.value;
+    let sortType;
+
+    if (input === "Upload Date: Old") {
+      sortType = this.sortAscending;
+    } else if (input === "Upload Date: New") {
+      sortType = this.sortDescending;
+    }
+    
+    let sorted = data.sort(sortType);
+    this.setState({gifs: sorted});
+  }
+
+  // old -> new
+  sortAscending(a, b) {
+    a = a.upload;
+    b = b.upload;
+
+    if (a > b) {
+      return 1;
+    } else if (a === b) {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+
+  // new -> old
+  sortDescending(a, b) {
+    a = a.upload;
+    b = b.upload;
+
+    if (a < b) {
+      return 1;
+    } else if (a === b) {
+      return 0;
+    } else {
+      return -1;
+    }
   }
 
   render() {
@@ -32,7 +117,7 @@ class App extends Component {
       <div className="App">
         <Nav />
         <DropNav />
-        <GifList data={this.state.gifs}/>
+        <GifList gifs={this.state.gifs} onSort={this.onSort}/>
       </div>
     );
   }
