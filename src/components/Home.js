29b74bfcfd495c4f8794b2e83e81df7de478d547
rthai <ahnamiko@gmail.com>
2018-10-18 04:47:34 -0700

    refactor as class comp, include trending fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React , { Component }from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import GifList from './GifList';
+import structureData from './lib/structureData.js';
 
-const Home = (props) => (
-  <GifList title='Trending GIFs' gifs={props.gifs} onSort={props.onSort} onSelectImage={props.onSelectImage}/>
-)
+
+// FIXME: console error when gif is clicked
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      gifs: [],
+    }
+  }
+
+  componentDidMount() {
+    const endpoint = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&limit=25&rating=G`;
+
+    axios.get(endpoint)
+      .then(response => {
+        let res = response.data;
+        let gifs = structureData(res.data);
+        this.setState({ gifs });
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <GifList title='Trending GIFs' gifs={this.state.gifs} onSelectImage={this.props.onSelectImage}/>
+    );
+  }
+}
 
 Home.propTypes = {
-  gifs: PropTypes.arrayOf(PropTypes.object),
-  onSort: PropTypes.func,
   onSelectImage: PropTypes.func,
 }
 
