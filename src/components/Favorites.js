fd7e7d582664f35d00bb5020d1062952c59f15d4
rthai <ahnamiko@gmail.com>
2018-10-17 17:23:48 -0700

    address FIXME, pass title prop

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -2,13 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import GifList from './GifList';
 
-// FIXME: what todo when favorite is removed, how is that updated in state? especially if clash with
-
 const Favorites = (props) => (
   <GifList title='Favorites' gifs={props.gifs} onSort={props.onSort} onSelectImage={props.onSelectImage}/>
 );
 
 Favorites.propTypes = {
+  title: PropTypes.string,
   gifs: PropTypes.arrayOf(PropTypes.object),
   onSort: PropTypes.func,
   onSelectImage: PropTypes.func,
