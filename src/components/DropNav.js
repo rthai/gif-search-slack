fd8fdf4e19a45c1c129c471103e2d5c427660380
rthai <ahnamiko@gmail.com>
2018-10-18 16:08:57 -0700

    add fav class

--- src/components/DropNav.js
+++ src/components/DropNav.js
@@ -7,7 +7,7 @@ import './styles/DropNav.css';
 const DropNav = (props) => (
   <ul className="drop-menu">
     <li className="link"><NavLink exact to="/">Home</NavLink></li>
-    <li className="link"><NavLink to="/favorites">Favorites{props.faves > 0 ? ` (${props.faves})` : ``}</NavLink></li>
+    <li className="link fav"><NavLink to="/favorites">Favorites{props.faves > 0 ? ` (${props.faves})` : ``}</NavLink></li>
     <li className="link"><NavLink to="/upload">Upload</NavLink></li>
   </ul>
 );
