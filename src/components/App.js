b8b6558ab85bbf2a1f9b98c86051f58e5720b515
rthai <ahnamiko@gmail.com>
2018-10-18 04:53:05 -0700

    move: structureData -> lib

--- src/components/App.js
+++ src/components/App.js
@@ -28,24 +28,6 @@ class App extends Component {
     this.onSelectImage = this.onSelectImage.bind(this);
   }
 
-  // structureData(data) {
-  //   return data.map(gif => {
-  //     let copy = {};
-  //     copy.src = gif.images.original.url;
-  //     // copy.thumbnail = gif.images.fixed_width.url;
-  //     copy.thumbnail = gif.images.preview_gif.url;
-  //     // copy.data_src = gif.images.fixed_width.url;
-  //     // copy.thumbnail = '';
-  //     copy.thumbnailWidth = parseInt(gif.images.preview_gif.width);
-  //     copy.thumbnailHeight = parseInt(gif.images.preview_gif.height);
-  //     copy.caption = gif.title;
-  //     copy.id = gif.id;
-  //     copy.user = gif.user;
-  //     copy.upload = gif.import_datetime;
-  //     return copy;
-  //   });
-  // }
-
   onSelectImage(title, index) {
     // console.log(index);
     var copyFavs = this.state.favorites.slice();
