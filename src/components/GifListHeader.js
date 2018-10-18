b6a9ec9c2c76f521368c2036883d6b69ce163073
rthai <ahnamiko@gmail.com>
2018-10-18 04:46:34 -0700

    remove implicit return

--- src/components/GifListHeader.js
+++ src/components/GifListHeader.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// FIXME: select doesnt go back default option after giflist updates
+// FIXME: there could be a bug if Trending GIFs and Favorites are searched for
+// FIXME: display total gif count for search conditional
 
-const GifListHeader = (props) => (
+const GifListHeader = (props) => {
+  
+  return (
   <div className="main-header">
     <h3>{props.title}</h3>
       <div className="sort">
-        <select className="sort-select" onChange={ (e) => props.onSort(props.title, e)}>
+        <select className="sort-select" onChange={ (e) => props.onSort(e)}>
           <option value="" defaultValue="disabled selected">Sort by</option>
           <option value="Date Added (oldest)">Date Added (oldest)</option> 
           <option value="Date Added (newest)">Date Added (newest)</option> 
         </select>
       </div>
   </div>
-)
+  );
+}
 
 GifListHeader.propTypes = {
   title: PropTypes.string,
   onSort: PropTypes.func,
+  pages: PropTypes.object
 };
 
 export default GifListHeader;
