3299c3736ec3bec25848fbc25d57a1c1c04800f5
rthai <ahnamiko@gmail.com>
2018-10-18 20:18:52 -0700

    lint

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink, Route } from 'react-router-dom';
 
 import './styles/Nav.css';
 import SearchBar from './SearchBar';
-
-// FIXME: active link styling doesnt work
 
 class Nav extends Component {
   constructor(props) {
@@ -16,7 +14,6 @@ class Nav extends Component {
   }
  
   showDropMenu = () => {
-    console.log('click')
     let menu = document.querySelector('.drop-menu');
     let show = this.state.showDropMenu;
 
@@ -30,7 +27,9 @@ class Nav extends Component {
   }
 
   toggle() {
-    this.setState({showDropMenu: !this.state.showDropMenu})
+    this.setState({
+      showDropMenu: !this.state.showDropMenu
+    });
   }
   
   render () {
@@ -43,11 +42,19 @@ class Nav extends Component {
             </button>
           </li>
           <li className="logo">
-            <h1><NavLink exact to="/">GIF Search</NavLink></h1>
+            <h1>
+              <NavLink exact to="/">GIF Search</NavLink>
+            </h1>
           </li>
-          <li className="link"><NavLink exact to="/">Home</NavLink></li>
-          <li className="link fav"><NavLink to="/favorites">Favorites{this.props.faves > 0 ? ` (${this.props.faves})` : ``}</NavLink></li>
-          <li className="link"><NavLink to="/upload">Upload</NavLink></li>
+          <li className="link">
+            <NavLink exact to="/">Home</NavLink>
+          </li>
+          <li className="link fav">
+            <NavLink to="/favorites">Favorites{this.props.faves > 0 ? ` (${this.props.faves})` : ``}</NavLink>
+          </li>
+          <li className="link">
+            <NavLink to="/upload">Upload</NavLink>
+          </li>
           <li>
             <div className="search-container">
               <Route component={SearchBar}/>
@@ -61,7 +68,6 @@ class Nav extends Component {
 
 Nav.propTypes = {
   faves: PropTypes.number,
-  // onSearch: PropTypes.func
 };
 
 export default Nav;
