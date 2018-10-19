b88edf0877a1be7c1a7302186851de9d3c72ed1f
rthai <ahnamiko@gmail.com>
2018-10-19 02:34:05 -0700

    refactor css with styled components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,8 +2,100 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink, Route } from 'react-router-dom';
 
-import './styles/Nav.css';
+import styled from 'styled-components';
 import SearchBar from './SearchBar';
+
+const NavWrapper = styled.nav`
+  background: #fff;
+  top:0;
+  width: 100%;
+  max-height: 63px;
+  transition:all 0.5s;
+  position: fixed;
+  z-index: 1000;
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  box-shadow: 0 0 5px rgba(0,0,0,.12);
+`;
+
+const NavLinksWrapper = styled.ul`
+  width: 90%;
+  margin: 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Hamburger = styled.li`
+  max-width: 0;
+  overflow: hidden;
+  transition: all 0.5s;
+  flex:1 2;
+  text-align: center;
+  @media (max-width: 470px) { 
+    min-width: 40px;
+    max-width: 40px;
+  }
+`;
+
+const MenuButton = styled.button`
+  background: #fff;
+  border: 0;
+  cursor: pointer;
+`;
+
+const LogoLi = styled.li`
+  flex:1 2;
+  text-align: center;
+  max-width: 200px;
+  transition: all 0.3s;
+  h1 {
+    min-width: 158px;
+    margin: 15px 0 15px 0;
+    background: #fff;
+    font-weight: 600;
+    font-size: 30px;
+    text-transform: uppercase;
+    @media (max-width: 970px) {
+      font-size: 28px;
+    }
+  }
+  a {
+    text-shadow: 1px 1px rgba(0,0,0,.12);
+  }
+  a:visited {
+    color: #000;
+  }
+  @media (max-width: 640px) { 
+    max-width: 0;
+    overflow: hidden;
+  }
+`;
+
+const LinkLi = styled.li`
+  flex:1 2;
+  text-align: center;
+  min-width: 100px;
+  max-width: 100px;
+
+  text-align: center;
+  a {
+    text-shadow: 1px 1px rgba(0,0,0,.12);
+  }
+  a:hover, a.active {
+    color: #1e90ff;
+  }
+  @media (max-width: 470px) { 
+    min-width: 0;
+    max-width: 0;
+    overflow: hidden;
+  }
+`;
+
+const SearchLi = styled.li`
+  flex: 2;
+`;
 
 class Nav extends Component {
   constructor(props) {
@@ -32,36 +124,37 @@ class Nav extends Component {
     });
   }
   
+  //TODO: CHECK hamburger button later
   render () {
     return (
-      <nav>
-        <ul className="nav-links">
-          <li className="hamburger">
-            <button className="menu" ref="hamburger" onClick={this.showDropMenu}>
+      <NavWrapper>
+        <NavLinksWrapper>
+          <Hamburger>
+            <MenuButton ref="hamburger" onClick={this.showDropMenu}>
               <i className="material-icons icn-menu">menu</i>
-            </button>
-          </li>
-          <li className="logo">
+            </MenuButton>
+          </Hamburger>
+          <LogoLi>
             <h1>
               <NavLink exact to="/">GIF Search</NavLink>
             </h1>
-          </li>
-          <li className="link">
+          </LogoLi>
+          <LinkLi>
             <NavLink exact to="/">Home</NavLink>
-          </li>
-          <li className="link fav">
+          </LinkLi>
+          <LinkLi>
             <NavLink to="/favorites">Favorites{this.props.faves > 0 ? ` (${this.props.faves})` : ``}</NavLink>
-          </li>
-          <li className="link">
+          </LinkLi>
+          <LinkLi>
             <NavLink to="/upload">Upload</NavLink>
-          </li>
-          <li>
+          </LinkLi>
+          <SearchLi>
             <div className="search-container">
               <Route component={SearchBar}/>
             </div>
-          </li>
-        </ul>
-      </nav>
+          </SearchLi>
+        </NavLinksWrapper>
+      </NavWrapper>
     );
   };
 }
