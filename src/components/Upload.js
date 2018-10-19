dc42a2a81d8084f536fc14e9a383b21f61398dd0
rthai <ahnamiko@gmail.com>
2018-10-18 20:17:04 -0700

    add url upload form

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './styles/Upload.css';
 
-const Upload = () => (
-  <div className="upload">
-    <h3>Upload</h3>
-  </div>
-);
+class Upload extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      upload: '',
+    };
+  }
+
+  preventDefaults = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  handleLink = (e) => {
+    this.preventDefaults(e);
+    let input = this.uploaded.value;
+    if (input === '') return;
+    this.setState({upload: input});
+  };
+
+  render() {
+    return (
+      <div className="upload">
+        <h3>Upload a GIF</h3>
+        <form className="upload-form" onSubmit={this.handleLink}>
+          <div className="upload-url">
+            <input type="text" placeholder=" Insert GIF URL" aria-label="insert gif url" ref={ input => this.uploaded = input} />
+            <button type="submit" className="btn-preview">Add GIF</button>
+          </div>
+        </form>
+        <div className="preview" ref={input => this.previewGallery = input}>
+          <img src={this.state.upload} alt=""/>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default Upload;
