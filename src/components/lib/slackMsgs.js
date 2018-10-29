43246f9cf24eb0e6e1129de099a37f1cebf147b2
rthai <ahnamiko@gmail.com>
2018-10-28 22:58:32 -0700

    create slack message bank

---
+++ src/components/lib/slackMsgs.js
@@ -0,0 +1,12 @@
+const messages = [
+  ":tada: A GIF a day keeps the blues away :tada:",
+  "I :heart: this GIF! ",
+  "I can't get enough of this GIF!",
+  "It is my human duty to share this GIF",
+  "You have to take a look at this GIF",
+  "me irl",
+  "So cute!",
+  "mood",
+];
+
+export default messages;
